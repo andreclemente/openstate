2385054bfdb0753368564bf1ec234e08b0f3395c
André Clemente <andre@openstate.andreclemente.dev>
2026-06-16 09:05:40 +0000

    fix: remove bare URL regex to prevent double-wrapping evidence links

--- docs/assets/js/github.v3.js
+++ docs/assets/js/github.v3.js
@@ -167,7 +167,6 @@ function renderMarkdown(text) {
     .replace(/^## (.+)$/gm, '<h2>$1</h2>')
     .replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>')
     .replace(/\[([^\]]+)\]\(([^)]+)\)/g, '<a href="$2" target="_blank" rel="noopener noreferrer">$1</a>')
-    .replace(/(https?:\/\/[^\s\)]+)/g, '<a href="$1" target="_blank" rel="noopener noreferrer">$1</a>')
     .replace(/^- (.+)$/gm, '<li>$1</li>')
     .replace(/(<li>.*<\/li>\n?)+/g, '<ul>$&</ul>')
     .replace(/\n\n/g, '</p><p>')
